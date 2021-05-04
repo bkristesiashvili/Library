fb5824ac15b20f5547ead5b1760ddbcc4b95b8a6
Besik Kristesiashvili <besysfx@gmail.com>
2021-05-04 23:18:41 +0400

    New User Registration form development

--- Library.WebApp/Controllers/AccountController.cs
+++ Library.WebApp/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using Library.Common;
 using Library.Common.Enums;
+using Library.Data.Entities;
+using Library.WebApp.Models;
 
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 using System;
@@ -14,6 +17,65 @@ namespace Library.WebApp.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly UserManager<User> userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
         public IActionResult Profile() => View();
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            ViewBag.AvailableRoles = GetAvailableRoles();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            ViewBag.AvailableRoles = GetAvailableRoles();
+
+            if (ModelState.IsValid)
+            {
+                var newUser = new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(newUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    if(!string.IsNullOrEmpty(model.Role) || !string.IsNullOrWhiteSpace(model.Role))
+                    {
+                        await userManager.AddToRoleAsync(newUser, model.Role);
+                        ViewBag.Success = "მომხმარებელი წარმატებით შეიქმნა.";
+                    }
+                }
+                else
+                    ViewBag.Error = "ასეთი მომხმარებელი უკვე არსებობს!";
+
+            }
+            return View(model);
+        }
+
+        private IEnumerable<string> GetAvailableRoles()
+        {
+            var availableRoles = new HashSet<string>(GlobalVariables.DefaultRoles.Count);
+
+            foreach (KeyValuePair<SystemDefaultRoles, string> item in GlobalVariables.DefaultRoles)
+                availableRoles.Add(item.Value);
+
+            return availableRoles;
+        }
     }
 }
---
+++ Library.WebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.WebApp.Models
+{
+    public sealed class RegisterViewModel
+    {
+        [Required(ErrorMessage ="სახელის ველი აუცილებელია!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "გვარის ველი აუცილებელია!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="ელ.ფოსტა აუცილებელია!")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email), ErrorMessage ="ელ.ფოსტა არ ემთხვევა!")]
+        [EmailAddress]
+        public string ConfirmEmail { get; set; }
+
+        [Required(ErrorMessage ="პაროლის ველი აუცილებელია!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="პაროლის დადასტურება აუცილებელია!")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="პაროლი არ ემთხვევა!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "როლის დამატება აუცილებელია!")]
+        public string Role { get; set; }
+
+    }
+}
