910a050187ac05dd09c3a07afba3bc9d568f4ac7
Besik Kristesiashvili <besysfx@gmail.com>
2021-06-02 00:30:22 +0400

    Develop PageFilter Partial view

---
+++ Library.WebApp/Models/FilterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.WebApp.Models
+{
+    public sealed class FilterViewModel
+    {
+        #region PUBLIC PROPERTIES
+
+        public string Url { get; set; }
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+
+        public string Search { get; set; }
+
+        public bool SelectDeleted { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public string Ordering { get; set; }
+
+        #endregion
+    }
+}
